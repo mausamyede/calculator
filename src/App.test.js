02ef2914d4a6157ee7ac523b7353eb7d79ca6d63
mausamyede <mausam.yede1@gmail.com>
2021-01-09 21:37:59 +0530

    Add snapshot tests and fix issue around websocket creation on deployment

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import App from "./App";
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({adapter: new Adapter()});
+
+describe('App', () => {
+    it('should render correctly', () => {
+        const component = shallow(<App/>);
+
+        expect(component).toMatchSnapshot();
+    });
 });
